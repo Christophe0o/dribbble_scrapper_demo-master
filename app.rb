d408405d48c24787aaf70e6318c9301b3fa62b8f
Christophe <christophe.lebec56@gmail.com>
2017-10-07 23:49:50 +0200

    xxxx

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ get '/search' do
   html_file = open(search_url)
   html_document = Nokogiri::HTML(html_file)
 
-  collection_css_path = 'html body' # <= customize
+  collection_css_path = 'html body .cover' # <= customize
   collection = html_document.css(collection_css_path)
 
   collection.each do |element|
