c74f2af79ecac633eebda73385e9a663c0ecb3f5
Christophe <christophe.lebec56@gmail.com>
2017-10-07 23:33:10 +0200

    xxxx

--- app.rb
+++ app.rb
@@ -9,17 +9,17 @@ end
 get '/search' do
   @cards = []
 
-  search_url = "https://dribbble.com/search?q=wagon" # <= customize
+  search_url = "https://www.behance.net/search?content=projects&sort=appreciations&time=week&search=fus%C3%A9e" # <= customize
 
   html_file = open(search_url)
   html_document = Nokogiri::HTML(html_file)
 
-  collection_css_path = 'li.group' # <= customize
+  collection_css_path = 'html body' # <= customize
   collection = html_document.css(collection_css_path)
 
   collection.each do |element|
     #title
-    title_css_path = '.dribbble .dribbble-shot .dribbble-img a.dribbble-over strong' # <= customize
+    title_css_path = '.cover .rf-project-cover__details a' # <= customize
     title = element.css(title_css_path).text
 
     @cards << [title]
