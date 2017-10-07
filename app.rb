4add6252304a9ce24416fe9a968e9a43606ef7e6
Christophe <christophe.lebec56@gmail.com>
2017-10-07 21:37:37 +0200

    zea

--- app.rb
+++ app.rb
@@ -23,23 +23,20 @@ get '/search' do
     title = element.css(title_css_path).text
 
     # skip if it's not a real card
-    #unless title.empty?
+    unless title.empty?
       # url
-      #relative_url_css_path = '' # <= customize
-      #relative_url = element.css(relative_url_css_path).attr('href')
-      #absolute_url = "#" # <= customize
+      relative_url_css_path = 'div.rf-project-cover__details a.rf-project-cover__title.js-project-cover-title-link' # <= customize
+      relative_url = element.css(relative_url_css_path).attr('href')
+      absolute_url = "https://www.behance.net#{relative_url}" # <= customize
       # or
       # absolute_url = "https://dribbble.com/" + relative_url
 
       # image
-      #image_url_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first a.rf-project-cover__image-container.js-project-cover-image-link.js-project-link img.rf-project-cover__image' # <= customize
-      #image_url = element.css(image_url_css_path).attr('src')
-
-      #@cards << [title, absolute_url, image_url]
-    #end
+      image_url_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first a.rf-project-cover__image-container.js-project-cover-image-link.js-project-link img.rf-project-cover__image' # <= customize
+      image_url = element.css(image_url_css_path).attr('src')
 
       @cards << [title, absolute_url, image_url]
-      
+    end
   end
 
   erb :'search'
