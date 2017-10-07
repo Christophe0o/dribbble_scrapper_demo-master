ebc530fb267ddf3d7e9ab1be7676fdf4712d965c
Christophe <christophe.lebec56@gmail.com>
2017-10-07 15:21:23 +0200

    scrapping

--- app.rb
+++ app.rb
@@ -14,25 +14,13 @@ get '/search' do
   html_file = open(search_url)
   html_document = Nokogiri::HTML(html_file)
 
-  collection_css_path = 'rf-project-cover rf-project-cover--project js-item js-project-cover qa-project-cover editable' # <= customize
+  collection_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first' # <= customize
   collection = html_document.css(collection_css_path) 
 
   collection.each do |element|
-    #title
-    title_css_path = '' # <= customize
-    title = element.css(title_css_path).text
-
-    # skip if it's not a real card
-    unless title.empty?
-      # url
-      relative_url_css_path = '' # <= customize
-      relative_url = element.css(relative_url_css_path).attr('href')
-      absolute_url = "https://dribbble.com/#{relative_url}" # <= customize
-      # or
-      # absolute_url = "https://dribbble.com/" + relative_url
-
+    
       # image
-      image_url_css_path = '.dribbble .dribbble-shot div.dribbble-img a.dribbble-link picture img' # <= customize
+      image_url_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first a.rf-project-cover__image-container.js-project-cover-image-link.js-project-link img.rf-project-cover__image' # <= customize
       image_url = element.css(image_url_css_path).attr('src')
 
       @cards << [title, absolute_url, image_url]
