6aa4e138a1c726d8083c55f9be17e01ffb831a2b
Christophe <christophe.lebec56@gmail.com>
2017-10-07 17:03:57 +0200

    test

--- app.rb
+++ app.rb
@@ -9,30 +9,30 @@ end
 get '/search' do
   @cards = []
 
-  search_url = "https://dribbble.com/search?q=wagon" # <= customize
+  search_url = "https://www.behance.net/search?content=projects&sort=appreciations&time=week&search=fus%C3%A9e" # <= customize
 
   html_file = open(search_url)
   html_document = Nokogiri::HTML(html_file)
 
-  collection_css_path = 'li.group' # <= customize
+  collection_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first' # <= customize
   collection = html_document.css(collection_css_path)
 
   collection.each do |element|
     #title
-    title_css_path = '.dribbble .dribbble-shot .dribbble-img a.dribbble-over strong' # <= customize
+    title_css_path = 'div.rf-project-cover__details a.rf-project-cover__title.js-project-cover-title-link' # <= customize
     title = element.css(title_css_path).text
 
     # skip if it's not a real card
-    unless title.empty?
+    #unless title.empty?
       # url
-      relative_url_css_path = '.dribbble .dribbble-shot .dribbble-img a.dribbble-over' # <= customize
-      relative_url = element.css(relative_url_css_path).attr('href')
-      absolute_url = "https://dribbble.com/#{relative_url}" # <= customize
+      #relative_url_css_path = '.dribbble .dribbble-shot .dribbble-img a.dribbble-over' # <= customize
+      #relative_url = element.css(relative_url_css_path).attr('href')
+      #absolute_url = "https://dribbble.com/#{relative_url}" # <= customize
       # or
       # absolute_url = "https://dribbble.com/" + relative_url
 
       # image
-      image_url_css_path = '.dribbble .dribbble-shot div.dribbble-img a.dribbble-link picture img' # <= customize
+      image_url_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first a.rf-project-cover__image-container.js-project-cover-image-link.js-project-link img.rf-project-cover__image' # <= customize
       image_url = element.css(image_url_css_path).attr('src')
 
       @cards << [title, absolute_url, image_url]
@@ -41,3 +41,6 @@ get '/search' do
 
   erb :'search'
 end
+
+div.rf-project-cover__details a.rf-project-cover__title.js-project-cover-title-link
+
