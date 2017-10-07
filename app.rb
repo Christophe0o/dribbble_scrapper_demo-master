3cc05e5359cb9f5cd3d64e14c0af1f7c7eadd1f0
Christophe <christophe.lebec56@gmail.com>
2017-10-07 22:19:39 +0200

    ahdgd

--- app.rb
+++ app.rb
@@ -14,28 +14,28 @@ get '/search' do
   html_file = open(search_url)
   html_document = Nokogiri::HTML(html_file)
 
-  collection_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first' # <= customize
+  collection_css_path = 'div.rf-project-cover' # <= customize
   collection = html_document.css(collection_css_path)
 
   collection.each do |element|
     #title
-    #title_css_path = 'div.rf-project-cover__details a.rf-project-cover__title.js-project-cover-title-link' # <= customize
+    #title_css_path = '' # <= customize
     #title = element.css(title_css_path).text
 
-      image_url_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first a.rf-project-cover__image-container.js-project-cover-image-link.js-project-link img' # <= customize
+      image_url_css_path = 'div.rf-project-cover a img' # <= customize
       image_url = element.css(image_url_css_path).attr('src')
 
     # skip if it's not a real card
     #unless title.empty?
       # url
-      #relative_url_css_path = 'div.rf-project-cover__details a.rf-project-cover__title.js-project-cover-title-link' # <= customize
+      #relative_url_css_path = '' # <= customize
       #relative_url = element.css(relative_url_css_path).attr('href')
       #absolute_url = "https://www.behance.net#{relative_url}" # <= customize
       # or
       # absolute_url = "https://dribbble.com/" + relative_url
 
       # image
-      #image_url_css_path = 'div.rf-project-cover.rf-project-cover--project.js-item.js-project-cover.qa-project-cover.editable.first a.rf-project-cover__image-container.js-project-cover-image-link.js-project-link img.rf-project-cover__image' # <= customize
+      #image_url_css_path = '' # <= customize
       #image_url = element.css(image_url_css_path).attr('src')
 
       #@cards << [title, absolute_url, image_url]
